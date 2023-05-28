bc583cee1fd031ecde5f8585f3cb9d45b950b743
saeedmdd <saeed.madadi7@gmail.com>
2023-05-28 11:14:07 +0330

    akbar

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/saeedmdd/go-hexa/cmd"
 	"github.com/saeedmdd/go-hexa/pkg/config"
+	"fmt"
 )
 
 func main() {
+	fmt.Println("akbar")
 	config.Apply()
 	cmd.Execute()
 }
